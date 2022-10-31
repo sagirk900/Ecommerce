2a71c03813ed7fd6e39cb3fbe164f56a6515c1de
vikas <vikasvishwakarma80@gmail.com>
2022-10-31 21:23:14 +0530

    commit

--- src/app/Main/User/Modules/product/inventories/inventories.component.ts
+++ src/app/Main/User/Modules/product/inventories/inventories.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inventories',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InventoriesComponent implements OnInit {
 
-  constructor() { }
+  selectedSubMenuId=1;
+  constructor(private router:Router) {
+    sessionStorage.setItem('pageTitle','Inventory');
+    sessionStorage.setItem('subMenu','Store Details');
+  }
 
   ngOnInit(): void {
+  }
+
+  getSubMenuTitle():string{
+    return <string>sessionStorage.getItem('subMenu');
+  }
+  changeSubMenu(menuId:number){
+    this.selectedSubMenuId = menuId;
+  }
+
+  addNewProduct(){
+    console.log("create order called");
+    this.router.navigateByUrl('User/Product/AddNewProduct');
   }
 
 }
